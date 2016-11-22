c9125a85d021521502423a6c26215744b4c9a220
Benoit Debordeaux <b.debordeaux2@gmail.com>
2016-11-22 16:09:55 +0100

    MovieController, newAction, showAction

--- src/CineProject/PublicBundle/Controller/MovieController.php
+++ src/CineProject/PublicBundle/Controller/MovieController.php
@@ -2,15 +2,47 @@
 
 namespace CineProject\PublicBundle\Controller;
 
+use CineProject\PublicBundle\Entity\Movie;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class MovieController extends Controller
 {
-    public function displayAllAction()
+    public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $movies = $em->getRepository('CineProjectPublicBundle:Movie')->findByIsActive(1);
+        $movies = $em->getRepository('CineProjectPublicBundle:Movie')->findByIsActive(true);
 
-        return $this->render('CineProjectPublicBundle:Movie:displayAll.html.twig', array('movies' => $movies));
+        return $this->render('CineProjectPublicBundle:Movie:index.html.twig', array('movies' => $movies));
+    }
+
+    public function newAction(Request $request)
+    {
+        $movie = new Movie();
+        $form = $this->createForm('CineProject\PublicBundle\Form\MovieType', $movie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($movie);
+            $em->flush($movie);
+
+            //return $this->redirectToRoute('actor_show', array('id' => $actor->getId()));
+        }
+
+        return $this->render('CineProjectPublicBundle:Movie:new.html.twig', array(
+            'movie' => $movie,
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function showAction(Movie $movie)
+    {
+        //$deleteForm = $this->createDeleteForm($movie);
+
+        return $this->render('CineProjectPublicBundle:Movie:show.html.twig', array(
+            'movie' => $movie,
+            //'delete_form' => $deleteForm->createView(),
+        ));
     }
 }
