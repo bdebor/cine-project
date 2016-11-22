3cec193347f5255f3b567e8ffa43935c01b70ac9
Benoit Debordeaux <b.debordeaux2@gmail.com>
2016-11-22 14:17:57 +0100

    Actor

---
+++ src/CineProject/PublicBundle/Entity/Actor.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace CineProject\PublicBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Actor
+ *
+ * @ORM\Table(name="actor")
+ * @ORM\Entity(repositoryClass="CineProject\PublicBundle\Repository\ActorRepository")
+ */
+class Actor
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fisrtName", type="string", length=255)
+     */
+    private $fisrtName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthDate", type="datetime")
+     */
+    private $birthDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="biography", type="text")
+     */
+    private $biography;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fisrtName
+     *
+     * @param string $fisrtName
+     * @return Actor
+     */
+    public function setFisrtName($fisrtName)
+    {
+        $this->fisrtName = $fisrtName;
+
+        return $this;
+    }
+
+    /**
+     * Get fisrtName
+     *
+     * @return string 
+     */
+    public function getFisrtName()
+    {
+        return $this->fisrtName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return Actor
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set birthDate
+     *
+     * @param \DateTime $birthDate
+     * @return Actor
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthDate
+     *
+     * @return \DateTime 
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * Set biography
+     *
+     * @param string $biography
+     * @return Actor
+     */
+    public function setBiography($biography)
+    {
+        $this->biography = $biography;
+
+        return $this;
+    }
+
+    /**
+     * Get biography
+     *
+     * @return string 
+     */
+    public function getBiography()
+    {
+        return $this->biography;
+    }
+}
