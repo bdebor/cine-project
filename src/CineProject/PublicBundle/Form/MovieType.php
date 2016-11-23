413493bbe54075ed730a2b0b855b3e9cefec73ec
Benoit Debordeaux <b.debordeaux2@gmail.com>
2016-11-23 12:10:57 +0100

    MovieType, deprecated terms correction

--- src/CineProject/PublicBundle/Form/MovieType.php
+++ src/CineProject/PublicBundle/Form/MovieType.php
@@ -2,7 +2,10 @@
 
 namespace CineProject\PublicBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,20 +17,22 @@ class MovieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', 'text', array(
+            ->add('title', TextType::class, array(
                 'required' => false
             ))
             ->add('description')
             ->add('releaseDate')
             //->add('visible');
-            ->add('visible','choice', array(
-                'choices' => array(0 => 'Non', 1 => 'Oui' ),
+            ->add('visible', ChoiceType::class, array(
+                'choices' => array('Non' => false, 'Oui' => true),
+                // always include this
+                'choices_as_values' => true,
                 'expanded' => true,
                 'multiple' => false,
             ))
-            ->add('actors','entity', array(
+            ->add('actors', EntityType::class, array(
                 'class' => 'CineProjectPublicBundle:Actor',
-                'property' => 'fullName',
+                'choice_label' => 'fullName',
                 'multiple' => true
             ));
     }
