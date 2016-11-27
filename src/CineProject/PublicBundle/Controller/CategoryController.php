b381102e1a20335015d878f8e2686a971d2285ae
Benoit Debordeaux <b.debordeaux2@gmail.com>
2016-11-27 18:55:43 +0100

    Category CRUD

---
+++ src/CineProject/PublicBundle/Controller/CategoryController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace CineProject\PublicBundle\Controller;
+
+use CineProject\PublicBundle\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Category controller.
+ *
+ */
+class CategoryController extends Controller
+{
+    /**
+     * Lists all category entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('CineProjectPublicBundle:Category')->findAll();
+
+        return $this->render('CineProjectPublicBundle:Category:index.html.twig', array(
+            'categories' => $categories,
+        ));
+    }
+
+    /**
+     * Creates a new category entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $category = new Category();
+        $form = $this->createForm('CineProject\PublicBundle\Form\CategoryType', $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('category_show', array('id' => $category->getId()));
+        }
+
+        return $this->render('CineProjectPublicBundle:Category:new.html.twig', array(
+            'category' => $category,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a category entity.
+     *
+     */
+    public function showAction(Category $category)
+    {
+        $deleteForm = $this->createDeleteForm($category);
+
+        return $this->render('CineProjectPublicBundle:Category:show.html.twig', array(
+            'category' => $category,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing category entity.
+     *
+     */
+    public function editAction(Request $request, Category $category)
+    {
+        $deleteForm = $this->createDeleteForm($category);
+        $editForm = $this->createForm('CineProject\PublicBundle\Form\CategoryType', $category);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('category_edit', array('id' => $category->getId()));
+        }
+
+        return $this->render('CineProjectPublicBundle:Category:edit.html.twig', array(
+            'category' => $category,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a category entity.
+     *
+     */
+    public function deleteAction(Request $request, Category $category)
+    {
+        $form = $this->createDeleteForm($category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($category);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('category_index');
+    }
+
+    /**
+     * Creates a form to delete a category entity.
+     *
+     * @param Category $category The category entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Category $category)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('category_delete', array('id' => $category->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
